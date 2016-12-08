4831c3149aaa8fd36353e18ca30e6ada52137685
Denis Morosenko <d.morosenko@cti.ru>
2016-12-08 16:31:08 +0300

    DB filter ui

--- src/com/calculator/aa/ui/FilterDB.java
+++ src/com/calculator/aa/ui/FilterDB.java
@@ -27,19 +27,11 @@ public class FilterDB extends JDialog {
     private FilterDB() {
         setContentPane(contentPane);
         setModal(true);
-        getRootPane().setDefaultButton(buttonOK);
+        getRootPane().setDefaultButton(buttonFilter);
 
-        buttonOK.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onOK();
-            }
-        });
+        buttonOK.addActionListener(e -> onOK());
 
-        buttonCancel.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        });
+        buttonCancel.addActionListener(e -> onCancel());
 
         // call onCancel() when cross is clicked
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
@@ -50,11 +42,8 @@ public class FilterDB extends JDialog {
         });
 
         // call onCancel() on ESCAPE
-        contentPane.registerKeyboardAction(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        contentPane.registerKeyboardAction(e -> onCancel(),
+                KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 
         spinnerMinHistory.addMouseWheelListener(new SpinnerWheelListener(spinnerMinHistory));
 
@@ -126,9 +115,9 @@ public class FilterDB extends JDialog {
                     .filter(filteredDatesList::contains)
                     .filter(filteredNamesList::contains);
 
-            Map<String, String> result = meta.getNames(allFiltered);
+            Map<String, String> resultNames = meta.getNames(allFiltered);
             DefaultListModel<String> resultModel = new DefaultListModel<>();
-            result.forEach((k, v) -> resultModel.addElement(k + " (" + v + ")"));
+            resultNames.forEach((k, v) -> resultModel.addElement(k + " (" + v + ")"));
             listResults.setModel(resultModel);
         });
     }
