06c6c826d130d0a0552a7c6d67848c3d02f589a5
Denis Morosenko <d.morosenko@cti.ru>
2016-12-27 13:33:30 +0300

    Bugfix

--- src/com/calculator/aa/ui/PortfolioChart.java
+++ src/com/calculator/aa/ui/PortfolioChart.java
@@ -114,6 +114,11 @@ class PortfolioChart extends JDialog {
                     toIndex[0] = data.length - 1;
                 }
 
+                if (toIndex[0] - fromIndex[0] < 2) {
+                    setCursor(Cursor.getDefaultCursor());
+                    return;
+                }
+
                 double[][] dataFiltered = Calc.filterValidData(data, maximals, fromIndex, toIndex);
 
                 if (dataFiltered == null) {
